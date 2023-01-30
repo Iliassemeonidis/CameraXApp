12c19dc895a951b9200f66306c26d5c5ea98b1b2
iliassemeonidis <paleologos@mail.ru>
2023-01-31 01:15:17 +0300

    работающая камера через usb

--- app/src/main/java/com/android/example/cameraxapp/MainActivity.kt
+++ app/src/main/java/com/android/example/cameraxapp/MainActivity.kt
@@ -1,18 +1,10 @@
 package com.android.example.cameraxapp
 
 import android.Manifest
-import android.content.BroadcastReceiver
-import android.content.Context
-import android.content.Intent
-import android.content.IntentFilter
 import android.content.pm.PackageManager
 import android.hardware.camera2.CameraCharacteristics
-import android.hardware.usb.UsbAccessory
-import android.hardware.usb.UsbManager
 import android.os.Build
 import android.os.Bundle
-import android.os.ParcelFileDescriptor
-import android.util.Log
 import android.widget.Toast
 import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AppCompatActivity
@@ -25,9 +17,6 @@ import androidx.core.content.ContextCompat
 import com.android.example.cameraxapp.databinding.ActivityMainBinding
 import com.google.common.util.concurrent.ListenableFuture
 import kotlinx.coroutines.*
-import java.io.FileInputStream
-import java.io.FileOutputStream
-import java.io.IOException
 import java.util.concurrent.ExecutionException
 import java.util.concurrent.ExecutorService
 import java.util.concurrent.Executors
@@ -42,60 +31,15 @@ class MainActivity : AppCompatActivity() {
     private lateinit var viewBinding: ActivityMainBinding
     private var imageCapture: ImageCapture? = null
     private lateinit var cameraExecutor: ExecutorService
-    private var bytes: ByteArray = byteArrayOf()
-    private val TIMEOUT = 0
-    private val forceClaim = true
-    lateinit var usbManager: UsbManager
 
+
+/*    lateinit var usbManager: UsbManager
     lateinit var accessory: UsbAccessory
     private var fileDescriptor: ParcelFileDescriptor? = null
     private var inputStream: FileInputStream? = null
-    private var outputStream: FileOutputStream? = null
+    private var outputStream: FileOutputStream? = null*/
 
-
-//
    /* private val usbReceiver = object : BroadcastReceiver() {
-
-        override fun onReceive(context: Context, intent: Intent) {
-            if (ACTION_USB_PERMISSION == intent.action) {
-                synchronized(this) {
-                    val device: UsbDevice? = intent.getParcelableExtra(EXTRA_DEVICE)
-
-                    if (intent.getBooleanExtra(UsbManager.EXTRA_PERMISSION_GRANTED, false)) {
-
-                        device?.getInterface(0)?.also { intf ->
-                            intf.getEndpoint(0)?.also { endpoint ->
-                                usbManager.openDevice(device)?.apply {
-                                    claimInterface(intf, forceClaim)
-                                    lifecycleScope.launch(
-                                        Dispatchers.Main
-                                                + SupervisorJob()
-                                                + CoroutineExceptionHandler{_,it ->Log.d("ERROR11", "${it.message}") }
-                                    ) {
-                                        withContext(Dispatchers.IO) {
-                                            bulkTransfer(endpoint, bytes, bytes.size, TIMEOUT) //do in another thread
-                                        }
-                                    }
-
-                                }
-                            }
-                        }
-//                        device?.apply {
-//
-//
-//                            //startCamera()
-//                        }
-                    } else {
-                        Log.d(TAG, "permission denied for device $device")
-                    }
-                }
-            }
-        }
-    }*/
-
-
-
-    private val usbReceiver = object : BroadcastReceiver() {
 
         override fun onReceive(context: Context, intent: Intent) {
             if (ACTION_USB_PERMISSION == intent.action) {
@@ -104,7 +48,7 @@ class MainActivity : AppCompatActivity() {
 
                     if (intent.getBooleanExtra(UsbManager.EXTRA_PERMISSION_GRANTED, false)) {
                         accessory.apply {
-                            openAccessory() //call method to set up accessory communication
+                           // openAccessory() //call method to set up accessory communication
                         }
                     } else {
                         Log.d(TAG, "permission denied for accessory $accessory")
@@ -112,7 +56,7 @@ class MainActivity : AppCompatActivity() {
                 }
             }
         }
-    }
+    }*/
 
 
     @RequiresApi(Build.VERSION_CODES.S)
@@ -121,16 +65,16 @@ class MainActivity : AppCompatActivity() {
         viewBinding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(viewBinding.root)
 
-   /*     println(packageManager.hasSystemFeature(PackageManager.FEATURE_CAMERA_EXTERNAL))
-        println(packageManager.hasSystemFeature(PackageManager.FEATURE_CAMERA_EXTERNAL))*/
+//        val filter = IntentFilter(ACTION_USB_PERMISSION)
+//        registerReceiver(usbReceiver, filter)
 
-        provide()
 
-      //  chekPermishAndStartCanera()
+
+        chekPermishAndStartCanera()
     }
 
     private fun chekPermishAndStartCanera() {
-        if (packageManager.hasSystemFeature(PackageManager.FEATURE_CAMERA_EXTERNAL)) {
+        if (allPermissionsGranted()) {
             startCamera()
         } else {
             ActivityCompat.requestPermissions(
@@ -142,7 +86,7 @@ class MainActivity : AppCompatActivity() {
     }
 
 
-    private fun openAccessory() {
+   /* private fun openAccessory() {
         Log.d(TAG, "openAccessory: $accessory")
         fileDescriptor = usbManager.openAccessory(accessory)
         fileDescriptor?.fileDescriptor?.also { fd ->
@@ -151,114 +95,13 @@ class MainActivity : AppCompatActivity() {
             val thread = Thread(  "AccessoryThread")
             thread.start()
         }
-    }
+    }*/
 
-    @RequiresApi(Build.VERSION_CODES.S)
-    private fun provide() {
-
-        val filter = IntentFilter(ACTION_USB_PERMISSION)
-        registerReceiver(usbReceiver, filter)
-
-//        usbManager = getSystemService(USB_SERVICE) as UsbManager
-//
-//        val deviceList: HashMap<String, UsbDevice> = usbManager.deviceList
-
-
-
-
-//        val permissionIntent = PendingIntent.getBroadcast(
-//            this,
-//            0,
-//            Intent(ACTION_USB_PERMISSION),
-//            PendingIntent.FLAG_MUTABLE
-//        )
-//
-//        deviceList.values.forEach { device ->
-//            usbManager.requestPermission(device, permissionIntent)
-//        }
-
-    }
-
-    //Варинат №1
-/*      private fun startCamera() {
-         val cameraProviderFuture = ProcessCameraProvider.getInstance(this)
-
-         cameraProviderFuture.addListener({
-             // Used to bind the lifecycle of cameras to the lifecycle owner
-             val cameraProvider = cameraProviderFuture.get()
-
-             // Preview
-             val preview = Preview.Builder()
-                 .build()
-                 .also {
-                     it.setSurfaceProvider(viewBinding.viewFinder.surfaceProvider)
-                 }
-
-             imageCapture = ImageCapture.Builder()
-                 .build()
-
-             // Select back camera as a default
-             val cameraSelector = CameraSelector.Builder().addCameraFilter(MyCameraFilter1("0")).build()
-
-             try {
-                 // Unbind use cases before rebinding
-                 cameraProvider.unbindAll()
-
-                 // Bind use cases to camera
-                 cameraProvider.bindToLifecycle(
-                     this,
-                     cameraSelector,
-                     preview,
-                     imageCapture
-                 )
-
-             } catch(exc: Exception) {
-                 Log.e(TAG, "Use case binding failed", exc)
-             }
-
-         }, ContextCompat.getMainExecutor(this))
-     }*/
-
-    //Варинат №2
-    /* private fun startCamera() {
-         val cameraProviderFuture = ProcessCameraProvider.getInstance(this)
-
-         val preview = Preview.Builder()
-             .build()
-             .also {
-                 it.setSurfaceProvider(viewBinding.viewFinder.surfaceProvider)
-             }
-         imageCapture = ImageCapture.Builder().build()
-         cameraProviderFuture.addListener(
-             {
-
-                 val cameraProvider = cameraProviderFuture.get()
-                 val cameraSelector =
-                     CameraSelector.Builder().addCameraFilter(MyCameraFilter1("0")).build()
-                 try {
-
-                     cameraProvider?.unbindAll()
-                     cameraProvider?.bindToLifecycle(this, cameraSelector, preview, imageCapture)
-
-                 } catch (e: ExecutionException) {
-                     e.printStackTrace()
-                     Toast.makeText(this, e.message, Toast.LENGTH_SHORT).show()
-                 } catch (e: InterruptedException) {
-                     e.printStackTrace()
-                     Toast.makeText(this, e.message, Toast.LENGTH_SHORT).show()
-                 } catch (e: IllegalArgumentException) {
-                     Toast.makeText(this, e.message, Toast.LENGTH_SHORT).show()
-                 }
-             }, ContextCompat.getMainExecutor(this)
-         )
-     }*/
-
-    //Варинат №3
     private fun startCamera() {
         val cameraProviderFuture: ListenableFuture<ProcessCameraProvider> =
             ProcessCameraProvider.getInstance(this)
 
-        imageCapture = ImageCapture.Builder().build();
+        imageCapture = ImageCapture.Builder().build()
         cameraProviderFuture.addListener({
             val cameraProvider: ProcessCameraProvider = cameraProviderFuture.get();
 
@@ -268,7 +111,6 @@ class MainActivity : AppCompatActivity() {
                     it.setSurfaceProvider(viewBinding.viewFinder.surfaceProvider)
                 }
 
-            println(packageManager.hasSystemFeature(PackageManager.FEATURE_CAMERA_EXTERNAL))
             val cameraSelector =
                 selectExternalOrBestCamera(cameraProvider)
 
@@ -292,67 +134,6 @@ class MainActivity : AppCompatActivity() {
         }, ContextCompat.getMainExecutor(this))
     }
 
-
-//    //Варинат №4
-    /*private fun startCamera() {
-        // Привязка к жц
-        val cameraProviderFuture = ProcessCameraProvider.getInstance(this)
-
-
-
-        cameraProviderFuture.addListener({
-
-            // Used to bind the lifecycle of cameras to the lifecycle owner
-            val cameraProvider: ProcessCameraProvider = cameraProviderFuture.get()
-
-            // Preview
-            val preview = Preview.Builder()
-                .build()
-                .also {
-                    it.setSurfaceProvider(viewBinding.viewFinder.surfaceProvider)
-                }
-
-
-            val imageAnalyzer = ImageAnalysis.Builder()
-                .build()
-                .also {
-                    it.setAnalyzer(cameraExecutor, LuminosityAnalyzer { luma ->
-                        Log.d(TAG, "Average luminosity: $luma")
-                    })
-                }
-
-
-            imageCapture = ImageCapture.Builder()
-                .build()
-
-            // Select camera
-            val cameraSelector = selectExternalOrBestCamera(cameraProvider)
-
-            try {
-                // Unbind use cases before rebinding
-                cameraProvider.unbindAll()
-
-                // Bind use cases to camera
-//                cameraProvider.bindToLifecycle(
-//                    this,
-//                    cameraSelector!!,
-//                    preview,
-//                    imageCapture,
-//                    imageAnalyzer
-//                )
-                cameraProvider.bindToLifecycle(
-                    this,
-                    cameraSelector!!,
-                    preview,
-                    imageCapture
-                )
-
-            } catch (exc: Exception) {
-                Log.e(TAG, "Use case binding failed", exc)
-            }
-
-        }, ContextCompat.getMainExecutor(this))
-    }*/
 
 
     private fun selectExternalOrBestCamera(provider: ProcessCameraProvider): CameraSelector? {
@@ -391,25 +172,6 @@ class MainActivity : AppCompatActivity() {
         cameraExecutor.shutdown()
 
     }
-
-//    override fun onRequestPermissionsResult(
-//        requestCode: Int, permissions: Array<String>, grantResults:
-//        IntArray
-//    ) {
-//        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
-//        if (requestCode == REQUEST_CODE_PERMISSIONS) {
-//            if (allPermissionsGranted()) {
-//        //        startCamera()
-//            } else {
-//                Toast.makeText(
-//                    this,
-//                    "Permissions not granted by the user.",
-//                    Toast.LENGTH_SHORT
-//                ).show()
-//                finish()
-//            }
-//        }
-//    }
 
 
     companion object {
